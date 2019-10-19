ab8cb4bdd38ba3cedc7a0b71350e1f00e0285cd3
LohanSolo <lohith400@gmail.com>
2019-10-19 19:31:02 +0530

    Initial submission
    The code for MVVXCross experiment

---
+++ TipCalculator/TipCalculator.Core/App.cs
@@ -0,0 +1,17 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using TipCalculator.Core.Services;
+using TipCalculator.Core.ViewModels;
+
+namespace TipCalculator.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
---
+++ TipCalculator/TipCalculator/Views/TipView.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using TipCalculator.Core.ViewModels;
+
+namespace TipCalculator.Views
+{
+    [Activity(Label = "Tip Calculator", MainLauncher = true)]
+    public class TipView : MvxActivity<TipViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.TipCalculatorView);
+        }
+    }
+}
