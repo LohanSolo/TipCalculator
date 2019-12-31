2f72b7fe61b5e449f32983009e7525f9a5fb5788
LohanSolo <lohith400@gmail.com>
2019-12-31 20:23:07 +0530

    UWP support

---
+++ TipCalculator/TipCalculator.UWP/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Platforms.Uap.Core;
+using MvvmCross.Platforms.Uap.Views;
+
+namespace TipCalculator.UWP
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public abstract class TipCalculatorApp : MvxApplication<MvxWindowsSetup<Core.App>, Core.App>
+    {
+    }
+}
---
+++ TipCalculator/TipCalculator.UWP/Views/TipView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Platforms.Uap.Views;
+using MvvmCross.ViewModels;
+using TipCalculator.Core.ViewModels;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TipCalculator.UWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    [MvxViewFor(typeof(TipViewModel))]
+    public sealed partial class TipView : MvxWindowsPage
+    {
+        public TipView()
+        {
+            InitializeComponent();
+        }
+    }
+}
